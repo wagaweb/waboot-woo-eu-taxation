0e429de331adbcd8d7ea1685f230c538918d6189
LostCore <lostcore@gmail.com>
2016-07-26 11:29:00 +0200

    Aggiunte le callback ajax per la validazione

--- src/includes/Plugin.php
+++ src/includes/Plugin.php
@@ -113,6 +113,176 @@ class Plugin extends TemplatePlugin {
 	}
 
 	/**
+	 * Validate a fiscal code
+	 *
+	 * @credit Umberto Salsi <salsi@icosaedro.it>
+	 *
+	 * @param string $fiscal_code
+	 *
+	 * @return array with 'is_valid' and 'err_message' keys.
+	 */
+	public function validate_fiscal_code($fiscal_code){
+		$fiscal_code = str_replace(' ', '', $fiscal_code);
+
+		$result = [
+			'is_valid' => false,
+			'err_message' => ''
+		];
+
+		if( $fiscal_code === '' ) {
+			$result['err_message'] = _x("The fiscal code is mandatory","WC Field Validation",$this->get_textdomain());
+			return $result;
+		}
+		if( strlen($fiscal_code) != 16 ) {
+			$result['err_message'] = _x( "La lunghezza del codice fiscale non &egrave;\n"
+			                             . "corretta: il codice fiscale dovrebbe essere lungo\n"
+			                             . "esattamente 16 caratteri.", "WC Field Validation", $this->get_textdomain() );
+
+			return $result;
+		}
+		$fiscal_code = strtoupper($fiscal_code);
+		if( preg_match("/^[A-Z0-9]+\$/", $fiscal_code) != 1 ){
+			$result['err_message'] = _x( "Il codice fiscale contiene dei caratteri non validi:\n"
+			                             ."i soli caratteri validi sono le lettere e le cifre.", "WC Field Validation", $this->get_textdomain() );
+			return $result;
+		}
+		$s = 0;
+		for( $i = 1; $i <= 13; $i += 2 ){
+			$c = $fiscal_code[$i];
+			if( strcmp($c, "0") >= 0 and strcmp($c, "9") <= 0 )
+				$s += ord($c) - ord('0');
+			else
+				$s += ord($c) - ord('A');
+		}
+		for( $i = 0; $i <= 14; $i += 2 ){
+			$c = $fiscal_code[$i];
+			switch( $c ){
+				case '0':  $s += 1;  break;
+				case '1':  $s += 0;  break;
+				case '2':  $s += 5;  break;
+				case '3':  $s += 7;  break;
+				case '4':  $s += 9;  break;
+				case '5':  $s += 13;  break;
+				case '6':  $s += 15;  break;
+				case '7':  $s += 17;  break;
+				case '8':  $s += 19;  break;
+				case '9':  $s += 21;  break;
+				case 'A':  $s += 1;  break;
+				case 'B':  $s += 0;  break;
+				case 'C':  $s += 5;  break;
+				case 'D':  $s += 7;  break;
+				case 'E':  $s += 9;  break;
+				case 'F':  $s += 13;  break;
+				case 'G':  $s += 15;  break;
+				case 'H':  $s += 17;  break;
+				case 'I':  $s += 19;  break;
+				case 'J':  $s += 21;  break;
+				case 'K':  $s += 2;  break;
+				case 'L':  $s += 4;  break;
+				case 'M':  $s += 18;  break;
+				case 'N':  $s += 20;  break;
+				case 'O':  $s += 11;  break;
+				case 'P':  $s += 3;  break;
+				case 'Q':  $s += 6;  break;
+				case 'R':  $s += 8;  break;
+				case 'S':  $s += 12;  break;
+				case 'T':  $s += 14;  break;
+				case 'U':  $s += 16;  break;
+				case 'V':  $s += 10;  break;
+				case 'W':  $s += 22;  break;
+				case 'X':  $s += 25;  break;
+				case 'Y':  $s += 24;  break;
+				case 'Z':  $s += 23;  break;
+			}
+		}
+		if( chr($s%26 + ord('A')) != $fiscal_code[15] ) {
+			$result['err_message'] = _x( "Il codice fiscale non &egrave; corretto:\n"
+			                             ."il codice di controllo non corrisponde.", "WC Field", $this->get_textdomain() );
+			return $result;
+		}
+		if (empty($result['err_message'])) {
+			$result['is_valid'] = true;
+			return $result;
+		}else{
+			$result['err_message'] = _x( "Si è verificato un errore inaspettato durante il controllo del codice fiscale", "WC Field Validation", $this->get_textdomain() );
+			return $result;
+		}
+	}
+
+	/**
+	 * Ajax callback to validate a fiscal code
+	 */
+	public function ajax_validate_fiscal_code(){
+		if(!defined("DOING_AJAX") || !DOING_AJAX) return;
+		$fiscal_code = isset($_POST['fiscal_code']) ? $_POST['fiscal_code'] : false;
+		if(!$fiscal_code){
+			echo json_encode([
+				'valid' => false,
+				'error' => __("Non è stato fornito un codice fiscale valido", $this->get_textdomain())
+			]);
+			die();
+		}
+		$result = $this->validate_fiscal_code($fiscal_code);
+		echo json_encode([
+			'valid' => $result['is_valid'],
+			'error' => $result['err_message']
+		]);
+		die();
+	}
+
+	/**
+	 * Validate an EU VAT number. Uses public EU API.
+	 * 
+	 * @param $vat
+	 *
+	 * @return bool
+	 */
+	public function validate_eu_vat($vat){
+		$countries = new \WC_Countries();
+		$cc = substr($vat, 0, 2);
+		$vn = substr($vat, 2);
+
+		$eu_countries = $countries->get_european_union_countries();
+
+		if (in_array($cc, $eu_countries)) {
+			$params = [
+				'countryCode' => $cc,
+				'vatNumber' => $vn
+			];
+
+			$client = new \SoapClient('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl');
+			$response = $client->__soapCall("checkVat", array($params) );
+
+			if(isset($response->valid) && $response->valid){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	/**
+	 * Ajax callback to validate an EU VAT
+	 */
+	public function ajax_validate_eu_vat(){
+		if(!defined("DOING_AJAX") || !DOING_AJAX) return;
+		$vat = isset($_POST['vat']) ? $_POST['vat'] : false;
+		if(!$vat){
+			echo json_encode([
+				'valid' => false,
+				'error' => __("Non è stata fornita una partita IVA valida", $this->get_textdomain())
+			]);
+			die();
+		}
+		$result = $this->validate_eu_vat($vat);
+		echo json_encode([
+			'valid' => $result,
+			'error' => !$result ? __("Non è stata fornita una partita IVA valida", $this->get_textdomain()) : ""
+		]);
+		die();
+	}
+
+	/**
 	 * Load the required dependencies for this plugin (called into parent::_construct())
 	 */
 	protected function load_dependencies() {
