6f4942fb1f3f78d1b5f51aac582fc88cbc10ede1
lostcore@gmail.com <123qw45>
2016-07-27 17:17:27 +0200

    Controllo sui check

--- src/frontend/Frontend.php
+++ src/frontend/Frontend.php
@@ -106,7 +106,7 @@ class Frontend {
 	 * @hooked 'woocommerce_price_ex_tax_amount'
 	 */
 	public function on_calculate_ex_tax_amount($tax_amount, $key, $rate, $price){
-		if(!$this->plugin->can_exclude_taxes($key)){
+		if($this->plugin->can_exclude_taxes($key)){
 			$tax_amount = 0; //WC does a sum of all applicable taxes. So by putting the "invalid" ones to 0, WC does not count them.
 		}
 		return $tax_amount;
